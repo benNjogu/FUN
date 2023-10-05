ebd232b6aacbc95a50d2979b91af5e096c50b8e2
benNjogu <kamandemachafi@gmail.com>
2023-10-05 13:58:21 +0300

    reversing a string and the valid parenthesis problem

--- DSA_DAILY/index.js
+++ DSA_DAILY/index.js
@@ -7,7 +7,7 @@ const maxNumber = (a) => Math.max(...a);
 
 console.log(maxNumber(a));
 
-//2.1 alternatively
+//2.1 alternatively (my way)
 let b = [3, 5, 24, 7, 8, 9, 13, 39];
 
 const maxOfB = (b) => {
@@ -20,3 +20,59 @@ const maxOfB = (b) => {
 };
 
 console.log('b', maxOfB(b));
+
+//3.0 pallindrome (my way)
+let _string = ' was it a car or a cat i saw';
+
+const isPallidrome = (_str) => {
+  let stack = [];
+  let que = [];
+
+  for(let i = 0; i < _str.length; i++) {
+    stack.push(_str[i])
+    que.unshift(_str[i]);
+  }
+
+  for(let j=0; j<_str.length; j++){
+    if(stack[j] !== que[j]) return false;
+    else return true;
+  }
+}
+
+if(isPallidrome(_string.toLowerCase().trim())) console.log(_string, ' is a pallidrome');
+else console.log(_string, ' is not a pallidrome');
+
+//3.1 Their way
+let _str = '[({[]})]';
+
+const pallid = (str) => str === str.split('').reverse().join('');
+
+console.log(pallid(_str.toLowerCase().trim()));
+
+//4.0 to reverse a given string
+const reverseString = (str) => {
+  return str.split('').reverse().join('')
+}
+
+console.log(reverseString('Hello'));
+
+//5.0 valid parenthesis problem
+const isValid = (str) => {
+  const stack = [];
+  const chars = {')': '(', ']': '[', '}': '{'};
+
+  for(const char of str) {
+    if(!chars[char]) {
+      stack.push(char);
+    } else if(stack.pop() !== chars[char]) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+}
+
+console.log(isValid('first', '[()]{}{[()()]()}'));
+console.log(isValid('second', '[()]{}'));
+
+//6.0 Take an array and return a new array with only the even numbers
